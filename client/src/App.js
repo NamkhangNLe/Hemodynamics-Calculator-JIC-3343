dbacfd606a6ba4ff6a0637e0344caf3b19bfc244
morganbroome <guymorgan100@gmail.com>
2023-11-20 21:58:40 -0500

    Added sidebar. Currently does not link to anything, and has dummy text. Future considerations will need to be made on if the nav bar goes above the sidebar, or if it should go to the right of the sidebar.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+
+import './styles/styles.css';
+
 /**
  * Renders the main application component.
  * @returns {JSX.Element} The App component
@@ -16,14 +20,32 @@ import Create from "./components/create";
  */
  const App = () => {
  return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/create" element={<Create />} />
-     </Routes>
-   </div>
+  <div className="app-container">
+    <Navbar />
+
+    <div className="content-container">
+      <Sidebar>
+        <Menu>
+          <SubMenu label="Patients">
+            <MenuItem> Create and View Patients </MenuItem>
+            <MenuItem> Medication Info </MenuItem>
+          </SubMenu>
+          <MenuItem> Calculate </MenuItem>
+          <MenuItem> Trends </MenuItem>
+          <MenuItem> Settings </MenuItem>
+          <MenuItem> Log Out </MenuItem>
+        </Menu>
+      </Sidebar>
+
+      <div className="main-content">
+        <Routes>
+          <Route exact path="/" element={<RecordList />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </div>
+    </div>
+  </div>
  );
 };
  export default App;
