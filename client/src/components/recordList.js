89501ff2ee86faaf981d1cf3ece5d450a895e74f
Dong, Eric <ericdong@gatech.edu>
2024-02-05 04:54:27 -0500

    Display patient notes in patient list
    Patient notes can be viewed in patient list.

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -8,6 +8,7 @@ const Record = (props) => (
    <td>{props.record.height}</td>
    <td>{props.record.weight}</td>
    <td>{props.record.medications}</td>
+   <td>{props.record.notes}</td>
    <td>
      <Link className="btn btn-link" to={`/view/${props.record._id}`}>View</Link> |
      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
@@ -89,6 +90,7 @@ export default function RecordList() {
             <th>Height</th>
             <th>Weight</th>
             <th>Medications</th>
+            <th>Notes</th>
           </tr>
         </thead>
         <tbody>{recordList()}</tbody>
