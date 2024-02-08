f1e5829c95711571b60af0153e53d5b6147e7249
Dong, Eric <ericdo16@outlook.com>
2024-02-08 06:42:48 -0500

    Add realtime output to Addition

--- client/src/components/addition.js
+++ client/src/components/addition.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/styles.css"
 
 
 export default function Addition({selectedPatientID}) {
     console.log("from", selectedPatientID)
-    const [val1, setVal1] = useState();
-    const [val2, setVal2] = useState();
+    const [val1, setVal1] = useState("");
+    const [val2, setVal2] = useState("");
+    // const [calculatedValue, setCalculatedValue] = useState("");
+    const [placeholderText, setPlaceholderText] = useState("Enter calculation inputs");
     const [form, setForm] = useState({
         valueType: "Addition",
         calculatedValue: ""
       });
-    //const [calculatedValue, setCalculatedValue] = useState();
 
-    function handleClick(e) {
-        // This prevents the form from being submitted when the calculate button is pressed.
-        e.preventDefault();
-
-        // Define formula logic
-        setForm({valueType: "Addition", calculatedValue: Number(val1) + Number(val2)});
-        console.log("HERE");
-    }
+    useEffect(() => {
+        setPlaceholderText((val1 === "" && val2 === "") ? "Enter calculation inputs" : "Missing inputs");
+        let output = (val1 !== "" && val2 !== "") ? +val1 + +val2 : "";
+        // setCalculatedValue(output);
+        setForm({valueType: "Addition", calculatedValue: output});
+    }, [val1, val2]);
 
     /**
      * Handles the form submission by sending a POST request to the server to create a new calculation.
@@ -29,6 +28,9 @@ export default function Addition({selectedPatientID}) {
     async function onSubmit(e) {
         e.preventDefault();
 
+        if (selectedPatientID === undefined) {
+            return;
+        }
         // Create a new object with the values from the form state.
         const newCalculation = {selectedPatientID: selectedPatientID, ...form};
         console.log("from front", newCalculation)
@@ -53,28 +55,27 @@ export default function Addition({selectedPatientID}) {
         setForm({valueType: "", calculatedValue: ""});
 
         } catch (error) {
-        console.error('Error submitting form:', error);
+            console.error('Error submitting form:', error);
         }
     }
 
     return (
         <div>
-                <h1>Addition of Values</h1>
-                <form onSubmit={onSubmit}>
-                    <div>
-                        Value 1: <input name="value1" type="number" value={val1} onChange={e => setVal1(e.target.value)}></input>
-                    </div>
-                    <div>
-                        Value 2: <input name="value2" type="number" value={val2} onChange={e => setVal2(e.target.value)}/>
-                    </div>
-                    <div>
-                        Output: <input type="text" value={form.calculatedValue} readOnly/>
-                    </div>
-                    <div>
-                        <button onClick={handleClick}>Calculate</button>
-                        <button type="submit">Save</button>
-                    </div>
-                </form>
+            <h1>Addition of Values</h1>
+            <form onSubmit={onSubmit}>
+                <div>
+                    Value 1: <input name="value1" type="number" step="1.0" value={val1} onChange={e => setVal1(e.target.value)}/>
+                </div>
+                <div>
+                    Value 2: <input name="value2" type="number" value={val2} onChange={e => setVal2(e.target.value)}/>
+                </div>
+                <div>
+                    Output: <input type="text" placeholder={placeholderText} value={form.calculatedValue} readOnly/>
+                </div>
+                <div>
+                    <button type="submit">Save</button>
+                </div>
+            </form>
         </div>
     );
-   }
+}
