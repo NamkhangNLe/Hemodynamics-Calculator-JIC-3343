8e1d7e496f7746c31f6ff9c55c578d4ee6ef7d5c
Sean Spych <114538345+thespych@users.noreply.github.com>
2024-03-02 14:31:49 -0500

    Update view.js imports

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function View() {
     const params = useParams();
