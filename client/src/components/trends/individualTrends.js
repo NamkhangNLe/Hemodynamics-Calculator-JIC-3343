e6f3681894a4dcfd1bae8fb05c6d2dc1a49839d0
Sean Spych <sspych3@gatech.edu>
2024-03-07 21:38:41 -0500

    Added individual trends charts.

--- client/src/components/trends/individualTrends.js
+++ client/src/components/trends/individualTrends.js
@@ -1,16 +1,51 @@
-import React from "react";
 import { Link , useParams } from 'react-router-dom';
+import TrendTableEntry from "./trendTableEntry"
+import React from "react";
+
 
 const IndividualTrends = () => {
     const { id } = useParams();
+
+    /**
+     * Renders the charts as a table, where each "table row" allows user to select
+     * whatever calculation they want to render.
+     */
+    function trendsTable() {
+        return (
+            <table style={{ marginTop: 20 }}>
+                <thead>
+                    <tr>
+                        <th>Individual Trends</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <TrendTableEntry id = {id} />
+                    </tr>
+                    <tr>
+                        <TrendTableEntry id = {id} />
+                    </tr>
+                    <tr>
+                        <TrendTableEntry id = {id} />
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+
+    const viewLink = `/view/${id}`;
+
     return(
         <div>
-        <Link to="/trends"> <button> View Other Patient Trends </button></Link>
-        <p>ID of Selected: {id}</p>
+            <div>
+                <Link to={viewLink}> <button> View Patient Profile</button></Link>
+                <Link to="/trends"> <button> View Other Patient Trends </button></Link>
+            </div>
+
+            {trendsTable()}
+
         </div>
     )
 }
 
 export default IndividualTrends
-
-
