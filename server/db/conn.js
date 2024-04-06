b7efbafc6224e3945885d4ab61e8597d32b754f2
Dong, Eric <ericdo16@outlook.com>
2024-04-06 07:32:43 -0400

    Revert "Update conn.js"
    This reverts commit cb03f9957bdbf41384dde56d21650f95adaf0719.

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,31 +1,26 @@
 const { MongoClient } = require("mongodb");
-
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, {
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-const db = client.db("hemodynamics-calculator");
+let _db;
 
 module.exports = {
     connectToServer: async function (callback) {
-        try {
-            // Connect the client to the server
-            await client.connect();
 
-            // Send a ping to confirm a successful connection
-            await client.db("admin").command({ ping: 1 });
-            console.log(
-                "Pinged your deployment. You successfully connected to MongoDB!"
-            );
-        } catch (err) {
-            console.error(err);
+        try {
+            await client.connect();
+        } catch (e) {
+            console.error(e);
         }
 
-        return (db === undefined ? false : true);
+        _db = client.db("hemodynamics-calculator");
+
+        return (_db === undefined ? false : true);
     },
     getDb: function () {
-        return db;
+        return _db;
     },
 };
