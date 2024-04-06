ec569413c4ecd3c9672871ff2f1c04df2ba61fbc
Dong, Eric <ericdo16@outlook.com>
2024-04-06 06:26:03 -0400

    Apply indentation formatting

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,26 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 let _db;
 
 module.exports = {
-  connectToServer: async function (callback) {
+    connectToServer: async function (callback) {
 
-    try {
-      await client.connect();
-    } catch (e) {
-      console.error(e);
-    }
+        try {
+            await client.connect();
+        } catch (e) {
+            console.error(e);
+        }
 
-    _db = client.db("hemodynamics-calculator");
+        _db = client.db("hemodynamics-calculator");
 
-    return (_db === undefined ? false : true);
-  },
-  getDb: function () {
-    return _db;
-  },
+        return (_db === undefined ? false : true);
+    },
+    getDb: function () {
+        return _db;
+    },
 };
