1402f436973ac95dd52af13fa8995c30ad341711
Dong, Eric <ericdo16@outlook.com>
2024-04-12 16:40:38 -0400

    Polish comments in calculation routes

--- server/routes/calculation.js
+++ server/routes/calculation.js
@@ -5,22 +5,22 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /calculation.
 const calcuationRoutes = express.Router();
 
-// This will help us connect to the database
+// This will help us connect to the database.
 const dbo = require("../db/conn");
 const db = dbo.getDb();
 
-// This help convert the id from string to ObjectId for the _id.
+// This will help us convert the id from String to ObjectId for the _id field.
 const ObjectId = require("mongodb").ObjectId;
 
 
-// This section will help you get a list of all the calculations.
+// This section will help us get a list of all the calculations.
 calcuationRoutes.get("/calculation", async (req, res) => {
     const collection = await db.collection("calculations");
     const results = await collection.find({}).toArray();
     res.send(results).status(200); // OK
 });
 
-// This section will help you get a list of all the calculations for a specific patient by patient_id.
+// This section will help us get a list of all the calculations for a specific patient by patient_id.
 calcuationRoutes.get("/calculation/:id", async (req, res) => {
     const query = { patient_id: req.params.id };
     const collection = await db.collection("calculations");
@@ -30,16 +30,16 @@ calcuationRoutes.get("/calculation/:id", async (req, res) => {
     else res.send(results).status(200); // OK
 });
 
-// This section will help you create a new record.
+// This section will help us create a new calculation.
 calcuationRoutes.post("/calculation/add", async (req, res) => {
-    try {
-        const newPatient = {
-            patient_id: req.body.selectedPatientID,
-            date: new Date(),
-            valueType: req.body.valueType,
-            calculatedValue: req.body.calculatedValue,
-        };
+    const newPatient = {
+        patient_id: req.body.selectedPatientID,
+        date: new Date(),
+        valueType: req.body.valueType,
+        calculatedValue: req.body.calculatedValue,
+    };
 
+    try {
         const collection = await db.collection("calculations");
         const result = await collection.insertOne(newPatient);
         res.send(result).status(204); // No Content
@@ -49,19 +49,19 @@ calcuationRoutes.post("/calculation/add", async (req, res) => {
     }
 });
 
-// Updates the date, valuetype/formula, and value for a calculation. Does NOT update associated patient_id.
+// This section will help us update the date, valuetype/formula, and value for a calculation by _id.
 calcuationRoutes.patch("/updatecalc/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const updates = {
+        $set: {
+            date: new Date(req.body.date),
+            valueType: req.body.valueType,
+            calculatedValue: req.body.calculatedValue
+        },
+    };
+
     try {
-        const query = { _id: new ObjectId(req.params.id) };
-
-        const updates = {
-            $set: {
-                date: new Date(req.body.date),
-                valueType: req.body.valueType,
-                calculatedValue: req.body.calculatedValue
-            },
-        };
-
         const collection = await db.collection("calculations");
         const result = await collection.updateOne(query, updates);
         res.send(result).status(200); // OK
@@ -71,10 +71,11 @@ calcuationRoutes.patch("/updatecalc/:id", async (req, res) => {
     }
 });
 
-// This section will help you delete a record.
+// This section will help us delete a record by _id.
 calcuationRoutes.delete("/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
     try {
-        const query = { _id: new ObjectId(req.params.id) };
         const collection = await db.collection("calculations");
         const result = await collection.deleteOne(query);
         res.send(result).status(200); // OK
