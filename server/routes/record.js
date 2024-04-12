6d9a8a1d9386f111742301a179e13534192ac3fd
Dong, Eric <ericdo16@outlook.com>
2024-04-12 16:31:22 -0400

    Polish comments in record routes

--- server/routes/record.js
+++ server/routes/record.js
@@ -9,18 +9,30 @@ const recordRoutes = express.Router();
 const dbo = require("../db/conn");
 const db = dbo.getDb();
 
-// This help convert the id from string to ObjectId for the _id.
+// This will help us convert the id from String to ObjectId for the _id field.
 const ObjectId = require("mongodb").ObjectId;
 
+/**
+ * Calculates age from a DOB given as a dateString in the format YYYY-MM-DD.
+ * @param {String} dob date of birth in YYYY-MM-DD format
+ * @returns age as the whole number of years since the DOB
+ */
+function dobToAge(dob) {
+    dob = new Date(dob); // dateString to Date object
+    const ageInMs = Date.now() - dob.getTime(); // millisecond difference of DOB and current date
+    const age = Math.abs(new Date(ageInMs).getUTCFullYear() - 1970); // calculate age in years
 
-// This section will help you get a list of all the records.
+    return age;
+}
+
+// This section will help us get a list of all the records.
 recordRoutes.get("/record", async (req, res) => {
     const collection = await db.collection("records");
     const results = await collection.find({}).toArray();
     res.send(results).status(200); // OK
 });
 
-// This section will help you get a record for a specific patient by patient_id.
+// This section will help us get a record for a specific patient by patient_id.
 recordRoutes.get("/record/:id", async (req, res) => {
     const collection = await db.collection("records");
     const query = { _id: new ObjectId(req.params.id) };
@@ -30,27 +42,22 @@ recordRoutes.get("/record/:id", async (req, res) => {
     else res.send(result).status(200); // OK
 });
 
-// This section will help you create a new record.
+// This section will help us create a new record.
 recordRoutes.post("/record/add", async (req, res) => {
-    const dob = new Date(req.body.dob); // Convert string to Date object
-    const ageDiffMs = Date.now() - dob.getTime(); // Get the difference in milliseconds
-    const ageDate = new Date(ageDiffMs); // Convert the age difference to a Date object
-    const age = Math.abs(ageDate.getUTCFullYear() - 1970); // Calculate the age
+    const newPatient = {
+        initials: req.body.initials,
+        dob: req.body.dob,
+        age: dobToAge(req.body.dob),
+        sex: req.body.sex,
+        height: req.body.height,
+        weight: req.body.weight,
+        bsa: Math.sqrt((+req.body.height * +req.body.weight) / 3600),
+        medications: req.body.medications,
+        notes: req.body.notes,
+        archived: false
+    };
 
     try {
-        const newPatient = {
-            initials: req.body.initials,
-            dob: req.body.dob,
-            age: age,
-            sex: req.body.sex,
-            height: req.body.height,
-            weight: req.body.weight,
-            bsa: Math.sqrt((+req.body.height * +req.body.weight) / 3600),
-            medications: req.body.medications,
-            notes: req.body.notes,
-            archived: false
-        };
-
         const collection = await db.collection("records");
         const result = await collection.insertOne(newPatient);
         res.send(result).status(204); // No Content
@@ -60,31 +67,26 @@ recordRoutes.post("/record/add", async (req, res) => {
     }
 });
 
-// This section will help you update a record by id.
+// This section will help us update a record by patient_id.
 recordRoutes.patch("/update/:id", async (req, res) => {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const updates = {
+        $set: {
+            initials: req.body.initials,
+            dob: req.body.dob,
+            age: dobToAge(req.body.dob),
+            sex: req.body.sex,
+            height: req.body.height,
+            weight: req.body.weight,
+            bsa: Math.sqrt((+req.body.height * +req.body.weight) / 3600),
+            medications: req.body.medications,
+            notes: req.body.notes,
+            archived: req.body.archived
+        },
+    };
+
     try {
-        const query = { _id: new ObjectId(req.params.id) };
-
-        const dob = new Date(req.body.dob); // Convert string to Date object
-        const ageDiffMs = Date.now() - dob.getTime(); // Get the difference in milliseconds
-        const ageDate = new Date(ageDiffMs); // Convert the age difference to a Date object
-        const age = Math.abs(ageDate.getUTCFullYear() - 1970); // Calculate the age
-
-        const updates = {
-            $set: {
-                initials: req.body.initials,
-                dob: req.body.dob,
-                age: age,
-                sex: req.body.sex,
-                height: req.body.height,
-                weight: req.body.weight,
-                bsa: Math.sqrt((+req.body.height * +req.body.weight) / 3600),
-                medications: req.body.medications,
-                notes: req.body.notes,
-                archived: req.body.archived
-            },
-        };
-
         const collection = await db.collection("records");
         const result = await collection.updateOne(query, updates);
         res.send(result).status(200); // OK
@@ -94,17 +96,19 @@ recordRoutes.patch("/update/:id", async (req, res) => {
     }
 });
 
-// This section will help you delete a record.
+// This section will help us delete a record by patient_id.
+// Calculations associated with the patient are also deleted.
 recordRoutes.delete("/:id", async (req, res) => {
+    const recordsQuery = { _id: new ObjectId(req.params.id) };
+    const calculationsQuery = { patient_id: req.params.id };
+
     try {
-        const recordsQuery = { _id: new ObjectId(req.params.id) };
         const recordsCollection = await db.collection("records");
         const result1 = await recordsCollection.deleteOne(recordsQuery);
         res.send(result1).status(200); // OK
 
-        const calculationsQuery = { patient_id: req.params.id };
         const calculationsCollection = await db.collection("calculations");
-        const result2 = await calculationsCollection.deleteMany(calculationsQuery);
+        await calculationsCollection.deleteMany(calculationsQuery);
     } catch (err) {
         console.error(err);
         res.status(500).send("Error deleting record"); // Internal Server Error
