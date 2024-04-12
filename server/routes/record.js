39a657e14db2aedee0d834183f6937c8431fb462
Dong, Eric <ericdo16@outlook.com>
2024-04-12 16:41:25 -0400

    Add period to comment

--- server/routes/record.js
+++ server/routes/record.js
@@ -5,7 +5,7 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
 
-// This will help us connect to the database
+// This will help us connect to the database.
 const dbo = require("../db/conn");
 const db = dbo.getDb();
 
