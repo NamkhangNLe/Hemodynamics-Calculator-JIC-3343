324b1516fdda8f3c5e726f2eabf66deaa32cf979
Dong, Eric <ericdo16@outlook.com>
2024-04-21 23:27:42 -0400

    Delete Navbar file

--- client/src/pages/framework.js
+++ client/src/pages/framework.js
@@ -2,7 +2,7 @@ import React from "react";
 import SidebarComp from "../components/sidebar";
 import '../styles/styles.css';
 
-// This page component includes the navbar and sidebar components.
+// This page component includes the sidebar component and the main page.
 // Takes in the main page component as a paramater.
 const PageFramework = ({ component }) => {
     return (
